19cb476ad804854bf3fd6845b742d78fa41305be
Richa277 <r.singh@axelor.com>
2023-02-21 14:24:32 +0530

    Fetching the data and representing it.

--- src/components/ContactsList.jsx
+++ src/components/ContactsList.jsx
@@ -1,15 +1,19 @@
 import styles from "./ContactsList.module.css";
+import profile from "../download.png"
 function ContactsList(props){
     return(
         <div className={styles.list}>
           {props.data.map((val,key)=>{
             return (
                 <div key={key} className={styles.details}>
-                    <span>{val.firstName}</span>
-                    <span>{val.lastName}</span>
-                    <span>{val.email}</span>
-                    <span>{val.fullName}</span>
-                    <span>{val.phone}</span>
+                  <div >
+                    <img src={profile} alt="profile" className={styles.profile}/>
+                  <p>{val.fullName}</p>
+                  </div>
+                    <div>
+                    <p>{val.email}</p>
+                    <p>{val.phone}</p>
+                    </div>
                 </div>
             )
           })}
