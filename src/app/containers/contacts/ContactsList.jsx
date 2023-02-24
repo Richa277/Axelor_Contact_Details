8b116d4e0cc8502d302e90e74a99813434e8ac26
Richa277 <r.singh@axelor.com>
2023-02-24 12:35:27 +0530

    Added loader for the contact list

--- src/app/containers/contacts/ContactsList.jsx
+++ src/app/containers/contacts/ContactsList.jsx
@@ -3,8 +3,8 @@ import profile from "../../../../src/assets/images/profile.png";
 import { useEffect, useState } from "react";
 import Form from "./Form";
 import { api } from "./Api";
-import { Route, Router, Routes } from "react-router";
 import { Link } from "react-router-dom";
+import Loader from "react-js-loader";
 function ContactsList() {
   const [data, setData] = useState([]);
   const [view, setView] = useState(false);
@@ -31,54 +31,73 @@ function ContactsList() {
   }, []);
   return (
     <div>
-      <button onClick={handleAddNewContact} className={styles.add}>
-        New Contact
-      </button>
-      {create ? (
-        <Link to="/form"><Form/></Link>
-      ) : view ? (
-        <Link to="/form"><Form data={data} id={selected} version={version} /></Link>
+      {data.length > 0 ? (
+        <>
+          <button onClick={handleAddNewContact} className={styles.add}>
+            New Contact
+          </button>
+          {create ? (
+            <Link to="/form">
+              <Form />
+            </Link>
+          ) : view ? (
+            <Link to="/form">
+              <Form data={data} id={selected} version={version} />
+            </Link>
+          ) : (
+            data.map((val) => {
+              return (
+                <div key={val.id} className={styles.details}>
+                  <div>
+                    <img
+                      src={profile}
+                      alt="profile"
+                      className={styles.profile}
+                    />
+                    <p>{val.fullName}</p>
+                    <button
+                      onClick={() => handleDeleteContact(val.id, val.version)}
+                      className={styles.delete}
+                    >
+                      Delete
+                    </button>
+                    <button
+                      onClick={() => handleEditContact(val.id, val.version)}
+                      className={styles.edit}
+                    >
+                      Edit
+                    </button>
+                  </div>
+                  <div>
+                    {val.email ? (
+                      <p>
+                        <span className={styles.email}>Email: </span>
+                        {val.email}
+                      </p>
+                    ) : (
+                      <p></p>
+                    )}
+                    {val.phone ? (
+                      <p>
+                        <span className={styles.phone}>Phone: </span>
+                        {val.phone}
+                      </p>
+                    ) : (
+                      <p></p>
+                    )}
+                  </div>
+                </div>
+              );
+            })
+          )}
+        </>
       ) : (
-        data.map((val) => {
-          return (
-            <div key={val.id} className={styles.details}>
-              <div>
-                <img src={profile} alt="profile" className={styles.profile} />
-                <p>{val.fullName}</p>
-                <button
-                  onClick={() => handleDeleteContact(val.id, val.version)}
-                  className={styles.delete}
-                >
-                  Delete
-                </button>
-                <button
-                  onClick={() => handleEditContact(val.id, val.version)}
-                  className={styles.edit}
-                >
-                  Edit
-                </button>
-              </div>
-              <div>
-                {val.email ? (
-                  <p>
-                    <span className={styles.email}>Email: </span>
-                    {val.email}
-                  </p>
-                ) : (
-                  <p></p>
-                )}
-                {val.phone ? (
-                  <p>
-                    <span className={styles.phone}>Phone: </span>
-                    {val.phone}
-                  </p>
-                ) : (
-                  <p></p>
-                )}
-              </div>
-            </div>
-          );
-        })
+        <Loader
+          type="spinner-circle"
+          bgColor={"green"}
+          title={"spinner-circle"}
+          size={200}
+        />
       )}
     </div>
   );
